28504c30920cf5e4af58d0d83cdfc333afc35deb
jiangxianhui <cnblogco@qq.com>
2022-06-16 13:11:03 +0800

    updata package.json

--- ce-admin/utils/redis.js
+++ ce-admin/utils/redis.js
@@ -1,16 +1,11 @@
-/*
- * @Author: Ghost
- * @Email: codeeetop@qq.com
- * @Date: 2022-03-15 13:30:24
- * @LastEditTime: 2022-04-09 09:41:37
- * @Description: ...每位新修改者自己的信息
- */
 
 let redis = require('redis')
 
 let $redis = {
   init: function (port = 6379, host = '127.0.0.1') {
     this.client = redis.createClient(port, host)
+    this.client.on('error', (err) => console.log('Redis Client Error', err));
+    this.client.connect();
     return this
   },
   set: function (key, val) {
@@ -33,20 +28,25 @@ let $redis = {
   // 回调方法
   callBack: function (rows = {}) {
     let { key = '', type = 'get', isObjects = false } = rows
+
     return new Promise((resolve, reject) => {
-      this.client[type](key, (err, val) => {
-        if (err) return reject(err)
+
+      this.client[type](key).then(val => {
         if (val == null) return resolve(null)
         try {
           resolve(!isObjects ? val : JSON.parse(val))
         } catch (error) {
           resolve(val)
         }
+      }, () => {
+        reject(val)
       })
+
     })
   }
-
 }
+
+
 
 let config = $redis.init()
 
